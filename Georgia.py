df2b2aac4c7c00c943714967b08eeecc8cbe53a7
hieppham8083 <72410825+hieppham8083@users.noreply.github.com>
2021-05-07 15:51:00 -0700

    Update Georgia.py

--- Georgia.py
+++ Georgia.py
@@ -26,10 +26,10 @@ def map():
     col1, col2 = st.beta_columns(2)
     if col1.button('Georgia Counties Votes Map'):
         #webbrowser.open('https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Total+Votes.html', new=2)
-        (https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Total+Votes.html)
+        [hello](https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Total+Votes.html)
     if col2.button('Georgia Counties Population Map'):
         #webbrowser.open('https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Population.html', new=2)
-        (https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Population.html)
+        [test](https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Population.html)
 def DEM_vs_REP():
     #fig, ax = plt.subplots()
     columns = ['year', 'DEM_Rate', 'REP_Rate']
