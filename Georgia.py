de46341330ef1f84da29fe2bdc09687550cdef7e
hieppham8083 <72410825+hieppham8083@users.noreply.github.com>
2021-05-07 15:59:41 -0700

    Update Georgia.py

--- Georgia.py
+++ Georgia.py
@@ -26,10 +26,10 @@ def map():
     import webbrowser
     col1, col2 = st.beta_columns(2)
     if col1.button('Georgia Counties Votes Map'):
-        webbrowser.open('https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Total+Votes.html', new=2)
+        webbrowser.open('https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Total+Votes.html', new=0)
        
     if col2.button('Georgia Counties Population Map'):
-        webbrowser.open('https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Population.html', new=2)
+        webbrowser.open('https://hieppham.s3.us-east-2.amazonaws.com/Final_project/georgia/Georgia+vs+South+Carolina+Population.html', new=0)
         
 def DEM_vs_REP():
     #fig, ax = plt.subplots()
