ae784178896bd621bc47a9261fc3c8bf1f58e4b5
hieppham8083 <72410825+hieppham8083@users.noreply.github.com>
2021-05-08 03:06:40 -0700

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -30,85 +30,88 @@ df = load_data()
 
 def epi_nn():
     global df
-    X = df.drop(['vep_turnout'],axis=1)
-    y = df['vep_turnout'].values
-    #splitting Train and Test 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
-    #standardization scaler - fit&transform on train, fit only on test
-    s_scaler = StandardScaler()
-    X_train = s_scaler.fit_transform(X_train.astype(np.float))
-    X_test = s_scaler.transform(X_test.astype(np.float))
+    try:
+        X = df.drop(['vep_turnout'],axis=1)
+        y = df['vep_turnout'].values
+        #splitting Train and Test 
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=101)
+        #standardization scaler - fit&transform on train, fit only on test
+        s_scaler = StandardScaler()
+        X_train = s_scaler.fit_transform(X_train.astype(np.float))
+        X_test = s_scaler.transform(X_test.astype(np.float))
 
-    # Multiple Liner Regression
-    regressor = LinearRegression()  
-    regressor.fit(X_train, y_train)
-    #evaluate the model (intercept and slope)
-    #print(regressor.intercept_)
-    #print(regressor.coef_)
-    #predicting the test set result
-    y_pred = regressor.predict(X_test)
-    #put results as a DataFrame
-    coeff_df = pd.DataFrame(regressor.coef_, df.drop('vep_turnout',axis =1).columns, columns=['Coefficient']) 
-    st.write(coeff_df)
+        # Multiple Liner Regression
+        regressor = LinearRegression()  
+        regressor.fit(X_train, y_train)
+        #evaluate the model (intercept and slope)
+        #print(regressor.intercept_)
+        #print(regressor.coef_)
+        #predicting the test set result
+        y_pred = regressor.predict(X_test)
+        #put results as a DataFrame
+        coeff_df = pd.DataFrame(regressor.coef_, df.drop('vep_turnout',axis =1).columns, columns=['Coefficient']) 
+        st.write(coeff_df)
 
-    # visualizing residuals
-    fig, ax = plt.subplots()
-    fig = plt.figure(figsize=(10,5))
-    residuals = (y_test- y_pred)
-    sns.distplot(residuals)
-    st.pyplot(fig)
+        # visualizing residuals
+        fig, ax = plt.subplots()
+        fig = plt.figure(figsize=(10,5))
+        residuals = (y_test- y_pred)
+        sns.distplot(residuals)
+        st.pyplot(fig)
 
 
-    y_pred = regressor.predict(X_test)
-    df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-    df1 = df.head(10)
-    st.write(df1)
+        y_pred = regressor.predict(X_test)
+        df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+        df1 = df.head(10)
+        st.write(df1)
 
-    # evaluate the performance of the algorithm (MAE - MSE - RMSE)
-    from sklearn import metrics
-    st.write('MAE:', metrics.mean_absolute_error(y_test, y_pred))  
-    st.write('MSE:', metrics.mean_squared_error(y_test, y_pred))  
-    st.write('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-    st.write('VarScore:',metrics.explained_variance_score(y_test,y_pred))
-    
-    # having 19 neuron is based on the number of available features
-    model = Sequential()
-    model.add(Dense(15,activation='relu'))
-    model.add(Dense(15,activation='relu'))
-    model.add(Dense(15,activation='relu'))
-    model.add(Dense(15,activation='relu'))
-    model.add(Dense(1))
-    model.compile(optimizer='Adam',loss='mse')
-    model.fit(x=X_train,y=y_train,
-          validation_data=(X_test,y_test),
-          batch_size=128,epochs=400)
-    model.summary() 
-    # predict values for the train and test sets
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
-    # score the training predictions with r2_score()
-    result = r2_score(y_train, y_train_pred)
-    st.write(f"r2_score = {result}")
+        # evaluate the performance of the algorithm (MAE - MSE - RMSE)
+        from sklearn import metrics
+        st.write('MAE:', metrics.mean_absolute_error(y_test, y_pred))  
+        st.write('MSE:', metrics.mean_squared_error(y_test, y_pred))  
+        st.write('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+        st.write('VarScore:',metrics.explained_variance_score(y_test,y_pred))
 
-    loss_df = pd.DataFrame(model.history.history)
-    loss_df.plot()
-    st.pyplot()
+        # having 19 neuron is based on the number of available features
+        model = Sequential()
+        model.add(Dense(15,activation='relu'))
+        model.add(Dense(15,activation='relu'))
+        model.add(Dense(15,activation='relu'))
+        model.add(Dense(15,activation='relu'))
+        model.add(Dense(1))
+        model.compile(optimizer='Adam',loss='mse')
+        model.fit(x=X_train,y=y_train,
+              validation_data=(X_test,y_test),
+              batch_size=128,epochs=400)
+        model.summary() 
+        # predict values for the train and test sets
+        y_train_pred = model.predict(X_train)
+        y_test_pred = model.predict(X_test)
+        # score the training predictions with r2_score()
+        result = r2_score(y_train, y_train_pred)
+        st.write(f"r2_score = {result}")
 
-    y_pred = model.predict(X_test)
-    # Visualizing Our predictions
-    fig = plt.figure(figsize=(9,6))
-    plt.scatter(y_test,y_pred)
-    # Perfect predictions
-    plt.plot(y_test,y_test,'r')
-    st.pyplot()
+        loss_df = pd.DataFrame(model.history.history)
+        loss_df.plot()
+        st.pyplot()
 
-    # visualizing residuals
-    fig, ax = plt.subplots()
-    fig = plt.figure()
-    residuals = (y_test- y_pred)
-    sns.distplot(residuals)
-    st.pyplot(fig)
-    return result
+        y_pred = model.predict(X_test)
+        # Visualizing Our predictions
+        fig = plt.figure(figsize=(9,6))
+        plt.scatter(y_test,y_pred)
+        # Perfect predictions
+        plt.plot(y_test,y_test,'r')
+        st.pyplot()
+
+        # visualizing residuals
+        fig, ax = plt.subplots()
+        fig = plt.figure()
+        residuals = (y_test- y_pred)
+        sns.distplot(residuals)
+        st.pyplot(fig)
+        return result
+    except:
+        pass
 
 #epi_nn()
 
