16ceceb8a034d2e90b846daa16e4fe88c852417f
hieppham8083 <72410825+hieppham8083@users.noreply.github.com>
2021-05-08 03:31:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ elif page == "Neural Network Model":
     #widget2.markdown("![Testing In Progress](https://hieppham.s3.us-east-2.amazonaws.com/Final_project/test.gif)")
     result = epi_nn()
     if result:
-        widget1.write("""## Training Model is done!""")
+        widget1.write(("<h3 style='text-align: center; color: blue;'>Training Model is done!</h3>", unsafe_allow_html=True))
         widget2 = st.empty()
         st.write("""### Training Model is done!""")
    
